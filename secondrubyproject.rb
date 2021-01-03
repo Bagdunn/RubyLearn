fba6c3ab2efe5122bd190ee90f0a0f23f9ba0e04
Bagdunn <ibodiko.lol.2@gmail.com>
2021-01-04 01:28:36 +0200

    Name and age

--- secondrubyproject.rb
+++ secondrubyproject.rb
@@ -1,3 +1,7 @@
 puts "hi, enter your name"
 name = gets.chomp()
-puts name
+puts ("Oh, hi, " + name)
+puts ("enter your birth year")
+year = gets.chomp()
+age = 2021 - year.to_i
+puts ("so, you are " + age.to_s + " years old ?")
